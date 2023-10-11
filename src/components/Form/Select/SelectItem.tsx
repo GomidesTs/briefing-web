5a126bc4e680cfcc5c7468320347219a3bfb3331
GomidesTs <gomidestulio5@gmail.com>
2023-10-11 10:13:47 -0300

    feat: select
    Created select component this will be responsible for making the user click on a predefined type of information only

---
+++ src/components/Form/Select/SelectItem.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import * as Select from '@radix-ui/react-select';
+import { Check } from 'lucide-react';
+
+export type SelectItemProps = Select.SelectItemProps & {
+  text: string;
+};
+
+export const SelectItem = ({ text, ...props }: SelectItemProps) => {
+  return (
+    <Select.Item
+      className="flex items-center justify-between gap-2 px-3 py-2.5 outline-none"
+      {...props}
+    >
+      <Select.ItemText asChild>
+        <span className="w-full cursor-pointer text-black">{text}</span>
+      </Select.ItemText>
+
+      <Select.ItemIndicator>
+        <Check className="h-4 w-4 text-blue-500" />
+      </Select.ItemIndicator>
+    </Select.Item>
+  );
+};
